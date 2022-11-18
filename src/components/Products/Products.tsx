75259c403198acab1e68e9db51779b3a389eee33
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-11-18 21:32:37 +0300

    done single products

---
+++ src/components/Products/Products.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import StarRatings from "react-star-ratings";
+import { FaHeart } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import ImageCard from "../../assets/category/beauty.jpg";
+
+const Products = () => {
+    return (
+        <div className="container pb-16">
+            <div className="grid grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
+                <Link to="/">
+                    <div className="card bg-base-100 shadow-xl group cursor-pointer">
+                        <figure className="h-72 relative">
+                            <div className="absolute top-3 rounded-full left-3 w-14 h-14 bg-success flex justify-center items-center flex-col">
+                                <span className="text-white -mb-2">Off</span>
+                                <span className="flex justify-center items-center text-white">
+                                    56%
+                                </span>
+                            </div>
+                            <ul className="transition duration-300 ease-in-out invisible flex absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 group-hover:visible">
+                                <li
+                                    className="py-3 px-3 rounded-lg ml-2 border-2 border-success hover:bg-transparent hover:text-white  text-white bg-success transition ease-in-out delay-15 cursor-pointer tooltip"
+                                    data-tip="Add To WishList"
+                                >
+                                    <FaHeart />
+                                </li>
+                                <li
+                                    className="py-3 px-3 rounded-lg ml-2 border-2 border-success hover:bg-transparent hover:text-white  text-white bg-success transition ease-in-out delay-15 cursor-pointer tooltip"
+                                    data-tip="Add To Cart"
+                                >
+                                    <FaShoppingCart />
+                                </li>
+                            </ul>
+                            <img
+                                className="h-full"
+                                src={ImageCard}
+                                alt="Shoes"
+                            />
+                        </figure>
+                        <div className="card-body pt-3 flex justify-center items-center">
+                            <p className="mt-1">Life Zed Air cx5</p>
+                            <div className="">
+                                <span className="mt-1 text-xl font-medium mr-2 text-gray-900">
+                                    $35
+                                </span>
+                                <span className="mt-2 line-through text-base font-medium text-accent">
+                                    $35
+                                </span>
+                            </div>
+                            <h2 className="card-title">
+                                <StarRatings
+                                    rating={5}
+                                    starRatedColor="orange"
+                                    numberOfStars={5}
+                                    name="rating"
+                                    starDimension="17px"
+                                    starSpacing="2px"
+                                />
+                            </h2>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../../components/Banner/Banner";
+import Products from "../../components/Products/Products";
 import Services from "../../components/Services/Services";
 import ShopByCategories from "../../components/ShopByCategories/ShopByCategories";
 import MainLayout from "../../layout/MainLayout";
@@ -10,7 +11,7 @@ const Home = () => {
             <Banner />
             <Services/>
             <ShopByCategories/>
-          
+            <Products/>
         </MainLayout>
     );
 };
